751b926f99061900228166a3c1033ab2edaf1005
pipelarra11 <pipelarra11@gmail.com>
2021-10-01 13:26:17 -0700

    5printfc

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+/* more headers goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+/* your code goes there */
+printf("with proper grammar, but the outcome is a piece of art, %d.\n ");
+return (0);
+}
