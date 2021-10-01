009bb4bf733c142287abf12f579de3097a2f73ef
pipelarra11 <pipelarra11@gmail.com>
2021-10-01 13:38:34 -0700

    5printfc

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -12,6 +12,6 @@
 int main(void)
 {
 /* your code goes there */
-printf("with proper grammar, but the outcome is a piece of art, \n ");
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
