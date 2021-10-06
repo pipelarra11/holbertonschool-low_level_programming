341103bc5eca77fb577169943ab361e33f7a1fc7
pipelarra11 <pipelarra11@gmail.com>
2021-10-06 08:25:13 -0700

    actualizado

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,5 +6,15 @@
  */
 int main(void)
 {
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+printf("size of a char: %d byte(s)\n", sizeof(d));
+printf("size of an int: %d byte(s)\n", sizeof(a));
+printf("size of a long int: %d byte(s)\n", sizeof(b));
+printf("size of a long long int: %d byte(s)\n", sizeof(c));
+printf("size of a float: %d byte(s)\n", sizeof(f));
 return (0);
 }
