4326b7cd1c4ef03d44beec398c2a54605ed3c860
pipelarra <pipelarra11@gmail.com>
2021-10-11 17:18:11 +0000

    ar1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swap the values of two integers
+ * @a: int pointer type
+ * @b: int pointer type
+ */
+void swap_int(int *a, int *b)
+{
+int temp = *a;
+*a = *b;
+*b = temp;
+}
