6b323199048c8284f62bc362433a4c6a33213ebc
pipelarra <pipelarra11@gmail.com>
2021-10-11 19:27:32 +0000

    p2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - print 10 times the alphabet, in lowercase.
+*
+*Return: Always 0.
+*/
+void print_alphabet_x10(void)
+{
+int j, i;
+
+for (j = 1; j <= 10; j++)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
